b9bcacc305b7ac640a6af2fcd8d07d6e37abddcd
Amderson Hernandez <ahernandezdep@gmail.com>
2024-02-29 13:53:31 -0500

    "changes"

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+const url = "https://platzi-avo.vercel.app/api/avo";
+//**Web Api Fetch */
+//* conectarnos al servidor
+window.fetch(url).then((response) => response.json()); //* procesar la respuesta y convertirla en JSON
 
-console.log('Happy hacking :)')
+//* obtener Json -> Data -> Renderizar info en el Browser
