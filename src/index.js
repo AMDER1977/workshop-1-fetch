38edeaa631449bd7fcb4c08c4c7583220c56103b
Amderson Hernandez <ahernandezdep@gmail.com>
2024-03-01 12:09:56 -0500

    "api intl"

--- src/index.js
+++ src/index.js
@@ -2,32 +2,69 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
-const baseUrl = "https://platzi-avo.vercel.app";
 
-//**Web Api Fetch */
-//* conectarnos al servidor
+const baseUrl = "https://platzi-avo.vercel.app";
+const appNode = document.querySelector("#app");
+
+// Intl
+// 1.- Format fechas
+// 2.- Format monedas
+
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat("es-MX", {
+    style: "currency",
+    currency: "MXN",
+  }).format(price);
+
+  return newPrice;
+};
+
+// web api
+
+// Conectarnos al server
 window
   .fetch(`${baseUrl}/api/avo`)
-  .then((response) => response.json()) //* procesar la respuesta y convertirla en JSON
-
+  .then((respuesta) => respuesta.json())
   .then((responseJson) => {
     const todosLosItems = [];
-    //* obtener Json -> Data -> Renderizar info en el Browser
-    responseJson.data.forEach((item) => {
-      //crear imagen, titulo y precio
-      const image = document.createElement("img");
-      image.src = `${baseUrl}${item.image}`;
 
+    responseJson.data.forEach((item) => {
+      // Crear imagen
+      const imagen = document.createElement("img");
+      imagen.src = `${baseUrl}${item.image}`;
+      imagen.className =
+        "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
+
+      // Crear título
       const title = document.createElement("h2");
+      title.className = "text-lg";
       title.textContent = item.name;
 
+      // Crear precio
       const price = document.createElement("div");
-      price.textContent = item.price;
+      price.className = "text-gray-600";
+      price.textContent = formatPrice(item.price);
 
-      const container = document.createElement("div");
-      container.append(image, title, price);
+      // Creamos un contenedor el título y el precio
+      const priceAndTitle = document.createElement("div");
+      priceAndTitle.className = "text-center md:text-left";
+      priceAndTitle.appendChild(title);
+      priceAndTitle.appendChild(price);
 
-      todosLosItems.push(container);
+      // Metemos todo dentro de una tarjeta contenedora
+      const card = document.createElement("div");
+      card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+      card.append(imagen, priceAndTitle);
+
+      // Metemos todo dentro del contenedor principal
+      const contenedor = document.createElement("div");
+      contenedor.appendChild(card);
+
+      todosLosItems.push(contenedor);
     });
-    document.body.append(...todosLosItems);
+
+    appNode.append(...todosLosItems);
   });
+
+// Procesar la respuesta y convertirla en JSON
+// JSON -> Data -> Renderizar info browser
