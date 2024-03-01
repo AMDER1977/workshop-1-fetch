f236aca2cd28f8dc9463fcb8bd310cdd815d6335
Amderson Hernandez <ahernandezdep@gmail.com>
2024-03-01 11:04:01 -0500

    "subiendo imagenes"

--- src/index.js
+++ src/index.js
@@ -2,24 +2,32 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
-const url = "https://platzi-avo.vercel.app/api/avo";
+const baseUrl = "https://platzi-avo.vercel.app";
+
 //**Web Api Fetch */
 //* conectarnos al servidor
 window
-  .fetch(url)
+  .fetch(`${baseUrl}/api/avo`)
   .then((response) => response.json()) //* procesar la respuesta y convertirla en JSON
 
   .then((responseJson) => {
+    const todosLosItems = [];
     //* obtener Json -> Data -> Renderizar info en el Browser
     responseJson.data.forEach((item) => {
       //crear imagen, titulo y precio
       const image = document.createElement("img");
-      document.body.appendChild(image);
+      image.src = `${baseUrl}${item.image}`;
 
       const title = document.createElement("h2");
-      document.body.appendChild(title);
+      title.textContent = item.name;
 
-      const price = document.createElement("span");
-      document.body.appendChild(price);
+      const price = document.createElement("div");
+      price.textContent = item.price;
+
+      const container = document.createElement("div");
+      container.append(image, title, price);
+
+      todosLosItems.push(container);
     });
+    document.body.append(...todosLosItems);
   });
